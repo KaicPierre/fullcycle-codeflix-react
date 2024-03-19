e557aad1eb07b034cbefd3be303c68d326892d58
Kaic Pierre <kaicpierresilva2002@gmail.com>
2024-03-18 23:11:01 -0300

    revert(reverts inter removal): remove inter from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import Header from './components/Header'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
