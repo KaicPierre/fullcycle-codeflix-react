f6a77b93b79a9bcf087bd2cbe8946f15343e213f
Kaic Pierre <kaicpierresilva2002@gmail.com>
2024-03-18 23:07:34 -0300

    feat(home page and header component): create home page base style and a header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,38 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image'
+import Header from './components/Header'
 
 export default function Home() {
   return (
-    <div className="relative h-full lg:h-[140vh]">
-      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
+    <div className="relative h-full overflow-hidden bg-gradient-to-b from-transparent to-black">
+      <Header></Header>
+      <main className="relative pb-24 pl-4  lg:pl-16">
         <div className="spave-y-2 flex flex-col py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-          <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black"></div>
-          <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
-            The Flash
-          </h1>
+          <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black">
+            <Image
+              src="/banner.png"
+              alt="Maid"
+              fill={true}
+              className="h-[65vh] object-cover object-top lg:h-[95vh]"
+            ></Image>
+          </div>
+          <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">Maid</h1>
 
           <p className="text-shadow-md masx-w-xs md:max-w-lg md:text-lg lg:max-w-2xl">
-            Após testemunhar o estranho assassinato de sua mãe e a injusta
-            acusação de seu pai pelo crime, Barry Allen fica sob os cuidados do
-            detetive Joe West e sua filha Iris West. Barry se torna brilhante,
-            mas socialmente, um cientista forense desconhecido trabalhando para
-            o Departamento de Polícia de Central City.
+            After fleeing an abusive relationship, a young mother finds a job
+            cleaning houses as she fights to provide for her child and build
+            them a better future. Watch all you want. Margaret Qualley stars
+            alongside real-life mom Andie MacDowell in this powerful drama
+            inspired by the bestselling memoir.
           </p>
         </div>
-        <button className="ms:w-32 w-24 rounded-full bg-white px-4 py-2 font-bold text-black lg:w-40">
-          Play
-        </button>
-        <button className="lg:w w-24 rounded-full bg-gray-700 px-4 py-2 font-bold text-white md:w-32 lg:w-40">
-          More info
-        </button>
+        <div className="flex space-x-4">
+          <button className=" intems-center cursor-pointer gap-x-2 rounded bg-white px-5 py-1.5 font-semibold text-black hover:opacity-75 md:px-8 md:py-2.5 md:text-xl">
+            Play
+          </button>
+          <button className=" intems-center cursor-pointer gap-x-2 rounded bg-gray-600 px-5 py-1.5 font-semibold text-white hover:opacity-75 md:px-8 md:py-2.5 md:text-xl">
+            More info
+          </button>
+        </div>
       </main>
     </div>
   )
